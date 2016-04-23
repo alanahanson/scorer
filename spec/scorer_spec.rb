dfafc5c15d58707de4eba1f3d2dfb24f27dc6adb
Alana Hanson <alanahanson@gmail.com>
2016-04-23 15:57:40 -0500

    Update scorer spec and remove driver code

--- spec/scorer_spec.rb
+++ spec/scorer_spec.rb
@@ -12,7 +12,9 @@ describe Scorer do
     file = double
     allow(Scorer).to receive(:open).and_return(file)
     allow(file).to receive(:read).and_return(data)
-    expect(Scorer.get_score(age: 12, income: 30294.21, zipcode: '53916')).to be_a(Scorer::Score)
+    scorer = Scorer.get_score(age: 12, income: 30294.21, zipcode: '53916')
+    expect(scorer).to be_a(Scorer::Score)
+    expect(scorer.propensity).to eq(0.26532)
+    expect(scorer.ranking).to eq("C")
   end
-
 end
